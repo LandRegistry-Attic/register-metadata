5c425e63d1f01a9aa109d77a0bbd2f3ec2d4c9f5
neilbutler <neil.butler@landregistry.gsi.gov.uk>
2015-08-12 10:24:33 +0100

    modifying how migration is run by using the models

--- application/models.py
+++ application/models.py
@@ -22,3 +22,22 @@ class mdref(db.Model):
     mdref = Column(String, primary_key=True)
     sequence = Column(Integer, primary_key=True)
     entry_id = Column(Integer, primary_key=True)
+
+class role(db.Model):
+
+    __tablename__ = 'role'
+
+    entry_role_code = Column(String, nullable=False, primary_key=True)
+    entry_role_seq_no = Column(Integer, nullable=True)
+    reg_child_code = Column(String, nullable=False)
+    cat_code = Column(String, nullable=False)
+    entry_role_desc = Column(String, nullable=False)
+    entry_status_code = Column(String, nullable=True)
+
+class subrole(db.Model):
+
+    __tablename__ = 'subrole'
+
+    sub_role_code = Column(String, nullable=False, primary_key=True)
+    entry_role_code = Column(String, nullable=False)
+    sub_role_desc = Column(String, nullable=False)
